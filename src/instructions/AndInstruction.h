79a926b4e7580ad38c11e5d05d5c7f7d70bb0741
Zakhar Karlin <zkarlin@smass.tech>
2020-11-16 15:27:01 +0300

    WIP: Add AndInstruction boilerplate code

---
+++ src/instructions/AndInstruction.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "interfaces/IInstruction.h"
+#include <common/Utils.h>
+
+namespace lc3_vm {
+/**
+ * \class AndInstruction
+ * \implements interfaces::IInstruction
+ * \brief implementation of bitwise AND instruction
+ */
+class AndInstruction : public interfaces::IInstruction
+{
+public:
+    /**
+     * \copydoc interfaces::IInstruction::operator()(common::Types::instruction_t)
+     */
+    void operator()(common::Types::instruction_t instruction) override
+    {
+        auto r0 = (instruction >> 9) & 0x7;
+        auto r1 = (instruction >> 6) & 0x7;
+        auto immFlag = (instruction >> 5) & 0x1;
+
+        if (immFlag) {
+            auto imm5 = common::Utils::signExtend(instruction & 0x1F, 5);
+            // todo: reg[r0] = reg[r1] & imm5;
+        } else {
+            auto r2 = instruction & 0x7;
+            // todo: reg[r0] = reg[r1] & reg[r2]
+        }
+
+        // todo: regMngr.updateFlag()
+    }
+
+    ~AndInstruction() override = default;
+};
+}
