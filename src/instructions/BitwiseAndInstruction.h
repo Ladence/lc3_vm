4e19edeace5b1bee6f92910af125186ec01bb5d5
Ladence <zakhar.karlin11@gmail.com>
2020-11-25 17:00:00 +0300

    complete BitwiseAndInstruction

--- src/instructions/BitwiseAndInstruction.h
+++ src/instructions/BitwiseAndInstruction.h
@@ -1,7 +1,10 @@
 #pragma once
 
-#include "interfaces/IInstruction.h"
 #include <common/Utils.h>
+
+#include "common/Singleton.h"
+#include "interfaces/IInstruction.h"
+#include "sysmanagement/RegisterManager.h"
 
 namespace lc3_vm {
 /**
@@ -17,19 +20,26 @@ public:
      */
     void operator()(common::Types::instruction_t instruction) override
     {
+        auto& regManager = common::Singleton<sysmanagement::RegisterManager>::get();
         auto r0 = (instruction >> 9) & 0x7;
         auto r1 = (instruction >> 6) & 0x7;
         auto immFlag = (instruction >> 5) & 0x1;
 
         if (immFlag) {
             auto imm5 = common::Utils::signExtend(instruction & 0x1F, 5);
-            // todo: reg[r0] = reg[r1] & imm5;
+
+            // reg[r0] = reg[r1] & imm5;
+            regManager.setVal(static_cast<hardware::RegistersSet>(r0),
+                              regManager.getVal(static_cast<hardware::RegistersSet>(r1)) & imm5);
         } else {
             auto r2 = instruction & 0x7;
-            // todo: reg[r0] = reg[r1] & reg[r2]
+            // reg[r0] = reg[r1] & reg[r2]
+            regManager.setVal(static_cast<hardware::RegistersSet>(r0),
+                              regManager.getVal(static_cast<hardware::RegistersSet>(r1)) &
+                                  regManager.getVal(static_cast<hardware::RegistersSet>(r2)));
         }
 
-        // todo: regMngr.updateFlag()
+        regManager.updateRCond(static_cast<hardware::RegistersSet>(r0));
     }
 
     ~BitwiseAndInstruction() override = default;
