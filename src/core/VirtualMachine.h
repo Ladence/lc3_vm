f01b1a8853f9db9ed65ac8cbead4d6822eeaa8cf
Ladence <zakhar.karlin11@gmail.com>
2020-11-25 16:51:04 +0300

    make "mutable" member functions of Virtual Machine virtual according to Template Method pattern

--- src/core/VirtualMachine.h
+++ src/core/VirtualMachine.h
@@ -13,26 +13,36 @@ namespace lc3_vm::core {
  */
 class VirtualMachine
 {
-    bool loadImageFile(const std::string& imageFilePath) noexcept;
-
-    common::Types::instruction_t fetchInstruction();
-
 public:
     /**
      * \brief Launches virtual machine's work
      */
     void launch();
-
     /**
      * \brief initializes resources for virtual machine
      * \return true if initialization has been succeed, otherwise false
      */
     bool boot();
-
     /**
      * \brief Stops virtual machine
      */
     void halt();
+
+protected:
+    /**
+     * \brief Fetches instruction using R_PC register
+     * \details virtual member function for overloading in case of extended Virtual Machine (Template Method pattern)
+     * \return next instruction to execute
+     */
+    virtual common::Types::instruction_t fetchInstruction();
+
+    /**
+     * \brief Loads binary image into virtual machine's memory
+     * \details virtual member function for overloading in case of extended Virtual Machine (Template Method pattern)
+     * \param imageFilePath path to binary image
+     * \return true if load succeed, otherwise false
+     */
+    virtual bool loadImageFile(const std::string& imageFilePath) noexcept;
 
 private:
     std::unique_ptr<interfaces::IInstructionExecutor> m_instructionExecutor;
